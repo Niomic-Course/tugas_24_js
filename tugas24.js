e57b6fa9e21a473d466376a5d65bc5f0d6dd51a1
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-09 23:54:29 +0800

    Tugas 24 JS Pengumpulan tugas 24 JS ke Niomic

---
+++ tugas24.js
@@ -0,0 +1,14 @@
+function urutBalik() {
+    const data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    console.log('Sebelumnya: ' + data);
+
+    // sort:
+    let ascend = data.sort();
+    console.log('Ascending: ' + ascend);
+
+    // reverse:
+    let descend = data.reverse();
+    console.log('Descending: ' + descend);
+}
+
+urutBalik();
